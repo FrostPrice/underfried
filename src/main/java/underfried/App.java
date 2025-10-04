f5212ea18e2cb9920fcf620b95ab1c1c51c71ca0
mateusbarbosa <barbosamat16@gmail.com>
2025-10-04 17:17:46 -0300

    feat: allow passage of arguments to agents

--- src/main/java/underfried/App.java
+++ src/main/java/underfried/App.java
@@ -1,17 +1,31 @@
 package underfried;
 
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+
 public class App {
     /**
      * Main method to launch JADE platform with it's agents
      */
     public static void main(String[] args) {
-        // Launch JADE with GUI and it's agents
-        String[] jadeArgs = {
-                "-gui",
-                "-agents", "testAgent:underfried.TestAgent" // MUST ADD YOUR AGENTS HERE
-        };
+        Restaurant restaurant = new Restaurant("Underfried Restaurant");
 
-        // Start JADE platform
-        jade.Boot.main(jadeArgs);
+        // Start JADE runtime
+        Runtime rt = Runtime.instance();
+        Profile p = new ProfileImpl();
+        p.setParameter(Profile.GUI, "true");
+        AgentContainer ac = rt.createMainContainer(p);
+
+        // Create agents with their necessary arguments
+        try {
+            Object[] chefArgs = new Object[] { restaurant };
+            AgentController chef = ac.createNewAgent("chef", "underfried.agents.Chef", chefArgs);
+            chef.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
