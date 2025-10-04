26b3085c7acdd263237c50f89adcca7d13b2d2c5
mateusbarbosa <barbosamat16@gmail.com>
2025-10-04 16:19:43 -0300

    feat: add restaurant menu

--- src/main/java/underfried/Restaurant.java
+++ src/main/java/underfried/Restaurant.java
@@ -1,5 +1,8 @@
 package underfried;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Restaurant {
     private Restaurant() {
     }
@@ -11,4 +14,75 @@ public class Restaurant {
 
     // Total plates to be washed
     public static int dirtyPlates = 0;
+
+    private Map<String, String[]> menu;
+    private String restaurantName;
+
+    public Restaurant(String name) {
+        this.restaurantName = name;
+        this.menu = new HashMap<>();
+        initializeMenu();
+    }
+
+    /**
+     * Initialize the restaurant menu with available dishes and their recipes
+     */
+    private void initializeMenu() {
+        // Main dishes
+        menu.put("super_meat_boy", new String[] { "meat", "tomato", "onion" });
+        menu.put("super_chicken_boy", new String[] { "chicken", "lettuce", "carrot" });
+        menu.put("steak", new String[] { "meat", "potato" });
+        menu.put("fish", new String[] { "fish", "carrot", "potato" });
+
+        // Pasta dishes
+        menu.put("pasta", new String[] { "pasta", "tomato" });
+        menu.put("carbonara", new String[] { "pasta", "meat", "onion" });
+
+        // Salads
+        menu.put("salad", new String[] { "lettuce", "tomato", "onion" });
+        menu.put("caesar", new String[] { "lettuce", "chicken", "tomato" });
+
+        // Soups
+        menu.put("soup", new String[] { "carrot", "potato", "onion" });
+        menu.put("chicken_soup", new String[] { "chicken", "carrot", "potato" });
+    }
+
+    /**
+     * Get the recipe for a specific dish
+     * 
+     * @param dishName the name of the dish
+     * @return array of ingredients needed, or null if dish doesn't exist
+     */
+    public String[] getRecipe(String dishName) {
+        return menu.get(dishName.toLowerCase());
+    }
+
+    /**
+     * Get all available dishes in the menu
+     * 
+     * @return a set of all dish names
+     */
+    public java.util.Set<String> getAvailableDishes() {
+        return menu.keySet();
+    }
+
+    /**
+     * Get the total number of dishes in the menu
+     * 
+     * @return number of dishes available
+     */
+    public int getMenuSize() {
+        return menu.size();
+    }
+
+    /**
+     * Print the entire menu to console
+     */
+    public void printMenu() {
+        System.out.println("=== " + restaurantName + " Menu ===");
+        for (Map.Entry<String, String[]> entry : menu.entrySet()) {
+            System.out.println(entry.getKey() + ": " + String.join(", ", entry.getValue()));
+        }
+        System.out.println("Total dishes: " + menu.size());
+    }
 }
