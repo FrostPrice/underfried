ef36402f2eda72b183a7f965f1d5cf3984e452cf
Jonathas Meine <jonhy.meine@gmail.com>
2025-10-04 16:16:18 -0300

    feat: add environment class Restaurant for global control

---
+++ src/main/java/underfried/Restaurant.java
@@ -0,0 +1,14 @@
+package underfried;
+
+public class Restaurant {
+    private Restaurant() {
+    }
+
+    public static int cleanPlates = 10;
+
+    // Total plates in use
+    public static int takenPlates = 10;
+
+    // Total plates to be washed
+    public static int dirtyPlates = 0;
+}
